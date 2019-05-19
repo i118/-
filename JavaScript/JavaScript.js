81fd03e6fbd530e996b621f7435e8ac00ab3408c
Сулейман <xasanovsuleyman@gmail.com>
2019-05-20 02:29:30 +0500

    Сделал первый switch case

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,23 @@
+let num = 70;
+
+// if (num > 60){
+//     console.log('so mutch');
+// }else if (num < 44){
+//     console.log('very small');
+// }else{
+//     console.log('good');
+// }
+
+switch (num) {
+    case num > 100 :
+        console.log('many');
+        break;
+    case num < 50 :
+        console.log('litle');
+        break;
+    case 70 :
+        console.log('very good');
+        break;
+    default :
+        console.log('nnnn');
+}
